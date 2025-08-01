9fd94981663a194fd6cd619ad61fb64ed7cddc26
Bluemi <bruno.schilling@protonmail.ch>
2025-08-01 21:14:18 +0200

    add container

--- abgeordnetenmap/src/routes/+page.svelte
+++ abgeordnetenmap/src/routes/+page.svelte
@@ -2,6 +2,18 @@
 </script>
 
 <style>
+    .container {
+        padding: 10px;
+        margin: auto;
+        width: 85%;
+        min-height: 100vh;
+        background-color: #dfe2e3; /* A dark gray */
+        @media (max-width: 768px) {
+            width: 100%;
+            margin: 0;
+        }
+    }
+
     nav {
         background-color: #e9ecef;
         padding: 1rem;
@@ -61,20 +73,23 @@
 	<nav>
 		<a href="/" class="nav-brand">AbgeordnetenMap</a>
 	</nav>
+	<div class="container">
 
-	<section class="search-section">
-		<div class="search-header">
-			<svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-					 stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-				<circle cx="11" cy="11" r="8"></circle>
-				<line x1="21" y1="21" x2="16.65" y2="16.65"></line>
-			</svg>
-			<div class="search-title">
-				<span class="search-title-small">Finde deine</span>
-				<span class="search-title-large">Fragen und Antworten</span>
+		<section class="search-section">
+			<div class="search-header">
+				<svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+						 stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+					<circle cx="11" cy="11" r="8"></circle>
+					<line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+				</svg>
+				<div class="search-title">
+					<span class="search-title-small">Finde deine</span>
+					<span class="search-title-large">Fragen und Antworten</span>
+				</div>
 			</div>
-		</div>
-		<input type="text" class="search-input" placeholder="Suche Thema">
-	</section>
+			<input type="text" class="search-input" placeholder="Suche Thema">
+		</section>
+
+	</div>
 </main>
 
