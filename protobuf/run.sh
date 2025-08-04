26c9e4af877cceb6f1b6f7fa1433dcd1eb028d35
Bluemi <bruno.schilling@protonmail.ch>
2025-08-04 13:51:58 +0200

    fix proto-bunde.js import

--- protobuf/run.sh
+++ protobuf/run.sh
@@ -10,4 +10,8 @@ protoc --proto_path=. --python_out=py ./questionbase.proto
 
 # generate typescript code
 mkdir -p js
+node_modules/.bin/pbjs -t static-module -w es6 -o js/proto-bundle.js questionbase.proto
 node_modules/.bin/pbjs -t static-module questionbase.proto | node_modules/.bin/pbts -o js/bundle.d.ts -
+
+# fix import
+sed -i 's/import \\* as \\$protobuf from/import \\$protobuf from/' js/proto-bundle.js
