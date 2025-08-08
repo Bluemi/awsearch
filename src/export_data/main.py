e24430ce4a3aea3625a48dfa65cf2254738b019a
Bluemi <bruno.schilling@protonmail.ch>
2025-08-08 10:02:52 +0200

    export complete data as json

--- src/export_data/main.py
+++ src/export_data/main.py
@@ -1,14 +1,28 @@
 import argparse
 import json
-from typing import Dict
+from typing import Dict, List, Optional
 from pathlib import Path
 
 import numpy as np
 from abgeordnetenwatch_python.models.questions_answers import QuestionAnswerResult
+from pydantic import BaseModel
 from tqdm import tqdm
 
 from data import LoadDossiers
 import questionbase_pb2
+
+
+class CompleteQuestion(BaseModel):
+    id: int
+    url: str
+    question: str
+    question_date: str
+    answer: Optional[str] = None
+    answer_date: Optional[str] = None
+
+
+class CompleteQuestionBase(BaseModel):
+    questions: List[CompleteQuestion]
 
 
 def get_args():
@@ -36,8 +50,8 @@ def main():
     cluster_ids = np.array([cluster[url] for url in urls])
 
     preview_questionbase = questionbase_pb2.PreviewQuestionBase()
-    complete_questionbase = questionbase_pb2.CompleteQuestionBase()
 
+    questions = []
     # export questions
     for index, (pos, url, cluster_id) in enumerate(zip(embeddings_2d, urls, cluster_ids)):
         # preview questions
@@ -59,16 +73,16 @@ def main():
             answer_text = qa.answer
             answer_date = qa.get_answer_date()
 
-        complete_question = questionbase_pb2.CompleteQuestion()
-        complete_question.id = index
-        complete_question.url = url
-        complete_question.question = question_text
-        complete_question.question_date = qa.get_question_date()
+        complete_question = {
+            'id': index, 'url': url, 'question': question_text, 'question_date': qa.get_question_date()
+        }
 
         if answer_text:
-            complete_question.answer = answer_text
-            complete_question.answer_date = answer_date
-        complete_questionbase.questions.append(complete_question)
+            complete_question['answer'] = answer_text
+            complete_question['answer_date'] = answer_date
+        questions.append(CompleteQuestion(**complete_question))
+
+    complete_questionbase = CompleteQuestionBase(questions=questions)
 
     # export clusters
     cluster_centers, topics = load_cluster_data(args, cluster_ids, embeddings_2d)
@@ -87,15 +101,15 @@ def main():
     preview_out_file = export_dir / 'preview_export.bin'
     with open(preview_out_file, 'wb') as f:
         f.write(buf)
-        buflen = len(buf)
-    print(f'Wrote {buflen} bytes to {preview_out_file}')
+        buffer_length = len(buf)
+    print(f'Wrote {buffer_length} bytes to {preview_out_file}')
 
-    buf = complete_questionbase.SerializeToString()
-    complete_out_file = export_dir / 'complete_export.bin'
-    with open(complete_out_file, 'wb') as f:
+    complete_out_file = export_dir / 'complete_export.json'
+    with open(complete_out_file, 'w') as f:
+        buf = complete_questionbase.model_dump_json()
         f.write(buf)
-        buflen = len(buf)
-    print(f'Wrote {buflen} bytes to {complete_out_file}')
+        buffer_length = len(buf)
+    print(f'Wrote {buffer_length} bytes to {complete_out_file}')
 
 
 def load_cluster_data(args, cluster_ids, embeddings_2d):
