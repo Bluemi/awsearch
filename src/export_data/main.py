a0a6ac81f094136eee804299fbe82ec864ede1c7
Bluemi <bruno.schilling@protonmail.ch>
2025-08-10 09:01:49 +0200

    improve json export

--- src/export_data/main.py
+++ src/export_data/main.py
@@ -1,9 +1,11 @@
 import argparse
 import json
-from typing import Dict, List, Optional
+import re
+from typing import Dict, List, Optional, Tuple
 from pathlib import Path
 
 import numpy as np
+from abgeordnetenwatch_python.models.politicians import Politician
 from abgeordnetenwatch_python.models.questions_answers import QuestionAnswerResult
 from pydantic import BaseModel
 from tqdm import tqdm
@@ -17,6 +19,9 @@ class CompleteQuestion(BaseModel):
     url: str
     question: str
     question_date: str
+    questioner: Optional[str] = None
+    respondent: Optional[str] = None
+    subject_area: Optional[str] = None
     answer: Optional[str] = None
     answer_date: Optional[str] = None
 
@@ -62,8 +67,9 @@ def main():
         preview_questionbase.questions.append(preview_question)
 
         # complete questions
-        qa = url_to_question[url]
+        qa, politician = url_to_question[url]
         question_text = qa.question or ''
+        extra_info = parse_question_string(question_text, politician.get_full_name())
         if qa.question_addition:
             question_text = question_text + '\n' + qa.question_addition
 
@@ -74,8 +80,12 @@ def main():
             answer_date = qa.get_answer_date()
 
         complete_question = {
-            'id': index, 'url': url, 'question': question_text, 'question_date': qa.get_question_date()
+            'id': index, 'url': url, 'question': question_text, 'question_date': qa.get_question_date(),
+            'politician': politician.get_full_name(), 'politician_url': politician.abgeordnetenwatch_url
         }
+        if extra_info is not None:
+            complete_question['questioner'] = extra_info['questioner']
+            complete_question['subject_area'] = extra_info['subject_area']
 
         if answer_text:
             complete_question['answer'] = answer_text
@@ -130,14 +140,80 @@ def load_cluster_data(args, cluster_ids, embeddings_2d):
     return cluster_centers, topics
 
 
-def load_url_to_question() -> Dict[str, QuestionAnswerResult]:
+def load_url_to_question() -> Dict[str, Tuple[QuestionAnswerResult, Politician]]:
     data_dir = Path('data/json/bundestag')
 
     url_to_question = {}
     for path, dossier in tqdm(LoadDossiers(data_dir), desc='load questions'):
         for qa in dossier.questions_answers.questions_answers:
-            url_to_question[qa.url] = qa
+            url_to_question[qa.url] = qa, dossier.politician
     return url_to_question
+
+def parse_question_string(text: str, politician_name: str) -> dict | None:
+    """
+    Parses a string of the format "Frage an <respondent> von <questioner>
+    bezüglich <subject_area>..." and extracts the relevant information.
+
+    :param text: The input string to parse.
+
+    :returns: A dictionary with keys 'respondent', 'questioner', and 'subject_area' if the text matches the pattern and
+    the subject is valid. Returns None otherwise.
+    """
+    valid_subjects = [
+        'Familie', 'Klima', 'Außenwirtschaft', 'Reaktorsicherheit', 'Geschäftsordnung', 'Immunität', 'Kultur', 'Medien',
+        'Medien, Kommunikation und Informationstechnik', 'Soziale Sicherung', 'Verbraucherschutz',
+        'Bildung und Erziehung', 'Technologiefolgenabschätzung', 'Forschung', 'Bundestag', 'Wahlprüfung', 'Verkehr',
+        'Innere Angelegenheiten', 'Verteidigung', 'Tourismus', 'Sport, Freizeit und Tourismus',
+        'Europapolitik und Europäische Union', 'Energie', 'Deutsche Einheit / Innerdeutsche Beziehungen (bis 1990)',
+        'Recht', 'Jugend', 'Landwirtschaft und Ernährung', 'Arbeit und Beschäftigung', 'Wirtschaft', 'Finanzen',
+        'Politisches Leben, Parteien', 'Senioren', 'Öffentliche Finanzen, Steuern und Abgaben',
+        'Gesellschaftspolitik, soziale Gruppen', 'Migration und Aufenthaltsrecht', 'Haushalt', 'Frauen',
+        'Entwicklungspolitik', 'Umwelt', 'Wissenschaft, Forschung und Technologie',
+        'Außenpolitik und internationale Beziehungen', 'Petitionen', 'Menschenrechte', 'Digitale Agenda',
+        'Innere Sicherheit', 'Lobbyismus & Transparenz', 'Staat und Verwaltung', 'Gesundheit', 'Humanitäre Hilfe',
+        'Naturschutz', 'digitale Infrastruktur', 'Sport', 'Raumordnung, Bau- und Wohnungswesen'
+    ]
+
+    pattern = fr"Frage an {politician_name} von (.*?) bezüglich (.*)"
+    match = re.match(pattern, text)
+
+    # Case 1: The overall pattern does not match the text.
+    if match:
+        questioner = match.group(1).strip()
+        subject_area = match.group(2).strip()
+    else:
+        pattern = fr"Frage an {politician_name} von (.*)"
+        match = re.match(pattern, text)
+        if match:
+            questioner = match.group(1).strip()
+            subject_area = None
+        else:
+            return None
+
+    if subject_area is not None:
+        found = False
+        for possible_area in valid_subjects:
+            if subject_area.startswith(possible_area):
+                subject_area = possible_area
+                found = True
+                break
+    else:
+        found = True
+
+    # Case 2: The pattern matched, but the subject area is not in the valid list.
+    if not found:
+        return None
+
+    # Case 3: A full, valid match.
+    return {
+        "questioner": questioner,
+        "subject_area": subject_area,
+    }
+
+
+def testy():
+    text = "Frage an Viola von Cramon-Taubadel von Drei französische S. bezüglich Europapolitik und Europäische Union"
+    parse_question_string(text, 'Viola von Cramon-Taubadel')
 
 
 if __name__ == '__main__':
