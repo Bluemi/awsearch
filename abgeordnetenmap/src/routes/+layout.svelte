f129a9dcaed7860ff93312bc4c038e297de04e86
Bluemi <bruno.schilling@protonmail.ch>
2025-08-15 08:45:43 +0200

    add favicon

--- abgeordnetenmap/src/routes/+layout.svelte
+++ abgeordnetenmap/src/routes/+layout.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
 	import '../app.css';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
+	<link rel="manifest" href="/favicon/site.webmanifest">
+	<link rel="icon" href="/favicon/favicon.ico">
 	<title>AbgeordnetenMap</title>
 </svelte:head>
 
